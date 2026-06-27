7addd4fa5787033c605b4c525e2fb977807223b6
Kevin Turcios <turcioskevinr@gmail.com>
2026-06-27 15:24:34 -0500

    Restore next type shape from known iteration value

--- nuitka/nodes/BuiltinNextNodes.py
+++ nuitka/nodes/BuiltinNextNodes.py
@@ -50,6 +50,11 @@ class ExpressionBuiltinNext1(ExpressionBuiltinSingleArgBase):
         return None
 
     def getTypeShape(self):
+        result = self.getIterationValue(0)
+
+        if result is not None:
+            return result.getTypeShape()
+
         result = self.getIterationValueShape(0)
 
         if result is None:
