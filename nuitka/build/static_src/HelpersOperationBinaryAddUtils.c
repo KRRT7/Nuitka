b9895114f4d19f0967d2b55ca4998ea60271fc1a
Kevin Turcios <turcioskevinr@gmail.com>
2026-06-29 05:56:37 -0500

    Avoid internal long sign flip helper

--- nuitka/build/static_src/HelpersOperationBinaryAddUtils.c
+++ nuitka/build/static_src/HelpersOperationBinaryAddUtils.c
@@ -76,7 +76,10 @@ static PyObject *LIST_CONCAT(PyThreadState *tstate, PyObject *operand1, PyObject
     }
 #define Nuitka_LongSetDigitSizeAndNegative(value, count, negative)                                                     \
     _PyLong_SetSignAndDigitCount(value, negative ? -1 : 1, count)
-#define Nuitka_LongFlipSign(value) _PyLong_FlipSign(value)
+#define Nuitka_LongFlipSign(value)                                                                                     \
+    ((PyLongObject *)value)->long_value.lv_tag =                                                                        \
+        (((PyLongObject *)value)->long_value.lv_tag & NON_SIZE_MASK) |                                                  \
+        (2 - (((PyLongObject *)value)->long_value.lv_tag & SIGN_MASK))
 #endif
 
 // Our version of _PyLong_New(size);
